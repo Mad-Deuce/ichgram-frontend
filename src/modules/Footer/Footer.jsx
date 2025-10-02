d54e7d90b31ed4960c7e80d939a429c7d803b1e2
zolotukhin_petro <zolotukhinpv@gmail.com>
2025-10-02 18:18:49 +0300

    finished footer

--- src/modules/Footer/Footer.jsx
+++ src/modules/Footer/Footer.jsx
@@ -1,5 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { selectModal } from "/src/redux/modal/modal-selectors";
+
+import {
+  showModal,
+  hideModal,
+  toggleModal,
+} from "/src/redux/modal/modal-slice";
+
+import IchgramLogo from "/src/shared/components/IchgramLogo/IchgramLogo";
+
+import Menu from "./Menu/Menu";
+
 import styles from "./Footer.module.css";
 
 export default function Footer() {
-  return <div className={styles.footer}>Footer</div>;
+  const dispatch = useDispatch();
+  const { childrenType } = useSelector(selectModal);
+
+  const toggleModalHandle = (title) => {
+    if (title === childrenType) {
+      dispatch(toggleModal());
+    } else {
+      dispatch(showModal(title));
+    }
+  };
+  const hideModalHandle = () => {
+    dispatch(hideModal());
+  };
+  return (
+    <div className={styles.footer}>
+      <Menu toggleModal={toggleModalHandle} hideModal={hideModalHandle} />
+      <Link to={"/copyright"} className={styles.copyright}>
+        {`${String.fromCodePoint(169)} 2024 ICHgram`}
+      </Link>
+    </div>
+  );
 }
---
+++ src/modules/Footer/Menu/Menu.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { selectUser } from "/src/redux/auth/auth-selectors";
+
+import menuItems from "./menuItems.js";
+
+import styles from "./Menu.module.css";
+
+export default function Menu({
+  className,
+  variant = "",
+  toggleModal = () => {},
+  hideModal = () => {},
+}) {
+  const fullClassName = `${styles.menu} ${className} ${styles[variant]}`;
+  const user = useSelector(selectUser);
+
+  const handleOnClick = (link, title) => {
+    if (link) {
+      hideModal();
+    } else {
+      toggleModal(title);
+    }
+  };
+
+  const elements = menuItems.map(({ title, icon, link }) => {
+    return (
+      <li
+        key={title}
+        className={
+          title === "Profile"
+            ? `${styles.item} ${styles.profileItem}`
+            : styles.item
+        }
+        onClick={() => handleOnClick(link, title)}
+      >
+        <Link to={link} className={styles.link}>
+          <p className={styles.title}>{title}</p>
+        </Link>
+        {!icon && <p className={styles.title}>{user.email}</p>}
+      </li>
+    );
+  });
+
+  return <ul className={fullClassName}>{elements}</ul>;
+}
